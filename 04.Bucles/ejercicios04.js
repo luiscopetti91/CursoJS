802f69f55dbfe0335d930469ed4abceb6200dedc
luiscopetti91 <106893472+luiscopetti91@users.noreply.github.com>
2023-06-26 12:22:34 -0300

    termino ejercicios bucles

--- 04.Bucles/ejercicios04.js
+++ 04.Bucles/ejercicios04.js
@@ -27,6 +27,7 @@ Finalmente, se retorna el valor de contador, que representa la cantidad de vocal
 function primerDivisible(numeroInicial, num) {
 // Encuentra el primer número divisible por 'num' a partir de un número dado utilizando un bucle while
 // Tu código:👇
+
 let i = numeroInicial;
 while (i % num !== 0) {
   i++;
@@ -36,7 +37,6 @@ return i;
 
 }
 
-/*"hola"))
 
 /*
 while (numeroInicial % num !==0) {
@@ -58,6 +58,17 @@ function encuentraX(x){
 //Realiza un algoritmo que recorre numeros del 1 al 100, cuando pase por el numero X pasado por parametros
 // retornar "El numero X es: ..."
 // Tu Código:👇
+for (let i = 1; i <= 100; i++) {
+  if (i === x) {
+    return "El numero X es: " + i;
+  }
+}
+return "No se encontro el numero";
+
+
+}
+
+/*function encuentraX(x){
 let i = 1;
 while (i <= 100) {
   if (i === x) {
@@ -66,8 +77,10 @@ while (i <= 100) {
   i++;
 }
 return "No se encontro el numero";
-
 }
+ */
+
+
 
 function sumandoTodo(num){
 // Realiza un algoritmo que el cual recorra del 0 al numero pasado por parametros, debes retornar la suma de todo los
@@ -90,6 +103,7 @@ function BinarioADecimal(num) {
 // La funcion recibe por parametro, un numero binario pero en tipo de dato String
 // debes tomar este numero binario y ayudandote de un bucle pasarlo a binario
 // tu código:👇
+
 let decimal = 0;
 const longitud = num.length;
 
@@ -99,16 +113,28 @@ for (let i = 0; i < longitud; i++) {
 }
 
 return decimal;
+
 }
+
 
 function Fibonacci(n){
 // Realiza una secuencia Fibonacci con el indice pasado por paramentro
 // Se puede representar mediante la siguiente fórmula: F(n) = F(n-1) + F(n-2)
 // Tu código:👇
-
-
+let arr = [0, 1];
+for (let i = 2; i < n; i++) {
+  arr.push(arr[i - 1] + arr[i - 2]);
 }
-
+return arr;
+}
+/*
+El código crea una función llamada Fibonacci que toma un parámetro n. La función genera una secuencia de Fibonacci con un índice igual al valor de n. Luego, la función devuelve la secuencia generada.
+Paso a paso:
+- La función Fibonacci toma un parámetro n que representa el índice de la secuencia Fibonacci que se desea generar.
+- Se declara un arreglo llamado arr con los valores iniciales de 0 y 1, que son los primeros dos números de la secuencia Fibonacci.
+- Se utiliza un bucle for que comienza en el índice 2 y continúa hasta n-1.
+- Dentro del bucle, se agrega al arreglo arr el resultado de la suma de los dos valores anteriores en la secuencia Fibonacci (es decir, arr[i-1] + arr[i-2]).
+- Finalmente, la función devuelve el arreglo arr que contiene la secuencia Fibonacci generada. */
 
 
 function esPrimo(numero) {
@@ -116,16 +142,44 @@ function esPrimo(numero) {
 //Un número primo es aquel que solo es divisible por 1 y por sí mismo.
 //Si el numero pasado es primo devolve: numero " es primo." , caso contrario: numero " no es primo."
 // Tu código:👇
-
+if (numero <= 1) {
+  return numero + " no es primo.";
 }
+for (let i = 2; i < numero; i++) {
+  if (numero % i === 0) {
+    return numero + " no es primo.";
+  }
+}
+return numero + " es primo.";
+}
+/*
+El código es una función llamada "esPrimo" que recibe un número como parámetro y verifica si es primo o no. Si el número es primo, devuelve el mensaje "numero es primo", de lo contrario devuelve "numero no es primo". 
+ Explicación paso a paso:
+ 1. La función "esPrimo" recibe un número como parámetro.
+2. Si el número es menor o igual a 1, devuelve el mensaje "numero no es primo" ya que los números menores o iguales a 1 no son considerados primos.
+3. Si el número es mayor que 1, se inicia un bucle "for" que se ejecutará desde 2 hasta el número anterior al número ingresado.
+4. Dentro del bucle, se verifica si el número ingresado es divisible por el número actual del bucle (i) utilizando el operador módulo (%). Si es divisible, significa que el número no es primo y se devuelve el mensaje "numero no es primo".
+5. Si el bucle termina sin encontrar ningún número por el cual el número ingresado sea divisible, significa que el número es primo y se devuelve el mensaje "numero es primo". */
+
+
+
 
 function invertirCadena(cadena) {
 //La función toma una cadena de texto como parámetro, devuelve la cadena invertida.
 //Ejemplo: 'Hola' -> devolver 'aloH'
 //Tu codigo:👇
-
+let cadenaInvertida = "";
+  for (let i = cadena.length - 1; i >= 0; i--) {
+    cadenaInvertida += cadena[i];
+  }
+  return cadenaInvertida;
 }
-  
+/*
+En este código, primero creamos una variable vacía llamada "cadenaInvertida". Luego, utilizamos un bucle "for" para recorrer la cadena de texto de atrás hacia adelante. En cada iteración del bucle, agregamos el carácter actual al comienzo de la variable "cadenaInvertida". Al final del bucle, devolvemos la variable "cadenaInvertida" como resultado.  
+Por ejemplo, si llamamos a la función "invertirCadena('Hola')", la función devolverá la cadena invertida "aloH" */  
+
+
+
 module.exports = {
     encuentraX,
     sumandoTodo,
